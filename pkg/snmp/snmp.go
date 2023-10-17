378be7662dec1e09b47d9d067528b2ae61d52e93
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-17 20:53:00 +0700

    add package snmp

--- pkg/snmp/snmp.go
+++ pkg/snmp/snmp.go
@@ -7,20 +7,19 @@ import (
 	"time"
 )
 
-// SetupSnmpConnection  for olt c320 using snmp v2c and gosnmp library
+// SetupSnmpConnection digunakan untuk membuat koneksi SNMP dan mengembalikan objek *gosnmp.GoSNMP
 func SetupSnmpConnection(config *config.Config) (*gosnmp.GoSNMP, error) {
 	target := &gosnmp.GoSNMP{
-		Target:    config.SnmpCfg.IpOlt,
+		Target:    config.SnmpCfg.Ip,
 		Port:      161,
 		Community: config.SnmpCfg.Community,
 		Version:   gosnmp.Version2c,
-		Timeout:   time.Duration(2) * time.Second,
-		//Logger:    log.New(os.Stdout, "", 0),
+		Timeout:   time.Duration(300) * time.Second,
 	}
 
 	err := target.Connect()
 	if err != nil {
-		return nil, fmt.Errorf("connect: %w", err)
+		return nil, fmt.Errorf("Gagal terhubung: %w", err)
 	}
 
 	return target, nil
