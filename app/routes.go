0fe621fbaf5115013320edf3e08a5c5a1e9cd591
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-18 21:31:46 +0700

    add routes

--- app/routes.go
+++ app/routes.go
@@ -5,31 +5,40 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
 	"github.com/sumitroajiprabowo/go-snmp-olt-c320/internal/handler"
 )
 
-func loadRoutes() *chi.Mux {
+func loadRoutes(ponHandler *handler.PonHandler) *chi.Mux {
 	router := chi.NewRouter()
 
+	// Middleware for logging requests
 	router.Use(middleware.Logger)
 
+	// Define a simple root endpoint
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, this is the root endpoint!"))
 	})
 
-	router.Route("/pon", loadPonRoutes)
+	// Route for "pon" related endpoints
+	router.Route("/pon", func(r chi.Router) {
+		// Use the "ponHandler" to handle specific endpoints
+		r.Get("/", ponHandler.List)
+		r.Get("/{id}", ponHandler.GetByPonID)
+	})
+
+	router.Route("/gtgo", func(r chi.Router) {
+		r.Get("/{gtgo_id}/pon/{id}", ponHandler.GetByGtGoIDAndPonID)
+	})
 
 	return router
 }
 
 func loadPonRoutes(router chi.Router) {
-	ponHandler := &handler.PonHanlder{}
+	ponHandler := &handler.PonHandler{}
+	gtGoHandler := &handler.PonHandler{}
 
-	router.Post("/", ponHandler.Create)
-	router.Get("/", ponHandler.List)
-	router.Get("/{id}", ponHandler.GetByID)
-	router.Get("/{id}/pon", ponHandler.GetByPonID)
-	router.Put("/{id}", ponHandler.UpdateByID)
-	router.Delete("/{id}", ponHandler.DeleteByID)
+	// Pass the "ponHandler" to the "loadRoutes" function
+	router.Mount("/", loadRoutes(ponHandler))
+	router.Mount("/", loadRoutes(gtGoHandler))
 }
