087758fd5f92effa0a8776dd74a876bf20e5a545
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-19 08:25:56 +0700

    add ONUCustomerInfo

--- internal/model/onu.go
+++ internal/model/onu.go
@@ -15,3 +15,17 @@ type ONUInfoPerGTGO struct {
 	RXPower      string `json:"rx_power"`
 	Status       string `json:"status"`
 }
+
+type ONUCustomerInfo struct {
+	GTGO         int    `json:"gtgo"`
+	PON          int    `json:"pon"`
+	ID           int    `json:"onu_id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	OnuType      string `json:"onu_type"`
+	SerialNumber string `json:"serial_number"`
+	RXPower      string `json:"rx_power"`
+	TXPower      string `json:"tx_power"`
+	Status       string `json:"status"`
+	IPAddress    string `json:"ip_address"`
+}
