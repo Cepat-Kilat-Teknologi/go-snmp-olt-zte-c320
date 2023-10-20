f6227a9e943d847f60c0974934015230d13b8df5
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-20 19:45:43 +0700

    update router

--- app/routes.go
+++ app/routes.go
@@ -4,15 +4,19 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	mdl "github.com/go-chi/chi/v5/middleware"
 	"github.com/megadata-dev/go-snmp-olt-c320/internal/handler"
+	"github.com/megadata-dev/go-snmp-olt-c320/internal/middleware"
 )
 
 func loadRoutes(onuHandler *handler.OnuHandler) *chi.Mux {
 	router := chi.NewRouter()
 
 	// Middleware for logging requests
-	router.Use(middleware.Logger)
+	router.Use(mdl.Logger)
+
+	// Middleware for CORS
+	router.Use(middleware.CorsMiddleware())
 
 	// Define a simple root endpoint
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +28,11 @@ func loadRoutes(onuHandler *handler.OnuHandler) *chi.Mux {
 		_ = write
 	})
 
-	// Route for "pon" related endpoints
-	router.Route("/pon", func(r chi.Router) {
-		// Use the "onuHandler" to handle specific endpoints
-		r.Get("/", onuHandler.List)
-	})
-
 	router.Route("/gtgo", func(r chi.Router) {
-		r.Get("/{gtgo_id}/pon/{id}", onuHandler.GetByGtGoIDAndPonID)
+		r.Get("/{gtgo_id}/pon/{pon_id}", onuHandler.GetByGtGoIDAndPonID)
+		//r.Get("/{gtgo_id}/pon/{pon_id}", onuHandler.GetByGtGoIDAndPonIDWithPaginate)
+		r.Get("/{gtgo_id}/pon/{pon_id}/onu/{onu_id}", onuHandler.GetByGtGoIDPonIDAndOnuID)
+		r.Get("/{gtgo_id}/pon/{pon_id}/empty", onuHandler.GetEmptyOnuID)
 	})
 
 	return router
