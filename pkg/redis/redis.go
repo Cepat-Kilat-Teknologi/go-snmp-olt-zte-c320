6cc47932e79a8927a0f482e54a4e88264a35bac2
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-20 19:44:44 +0700

    add redis connection

---
+++ pkg/redis/redis.go
@@ -0,0 +1,18 @@
+package redis
+
+import (
+	"github.com/megadata-dev/go-snmp-olt-c320/config"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+func NewRedisClient(cfg *config.Config) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:         cfg.RedisCfg.Host + ":" + cfg.RedisCfg.Port,
+		Password:     cfg.RedisCfg.Password,
+		DB:           cfg.RedisCfg.DB,
+		MinIdleConns: cfg.RedisCfg.MinIdleConnections,
+		PoolSize:     cfg.RedisCfg.PoolSize,
+		PoolTimeout:  time.Duration(cfg.RedisCfg.PoolTimeout) * time.Second,
+	})
+}
