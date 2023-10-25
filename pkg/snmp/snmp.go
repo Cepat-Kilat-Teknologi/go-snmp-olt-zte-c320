6c3c30a97ea09b1edcd1f67f810aa797a8c64118
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-25 11:16:58 +0700

    add logger

--- pkg/snmp/snmp.go
+++ pkg/snmp/snmp.go
@@ -4,23 +4,37 @@ import (
 	"fmt"
 	"github.com/gosnmp/gosnmp"
 	"github.com/megadata-dev/go-snmp-olt-zte-c320/config"
+	"log"
 	"time"
 )
 
-// SetupSnmpConnection digunakan untuk membuat koneksi SNMP dan mengembalikan objek *gosnmp.GoSNMP
+// SetupSnmpConnection is a function to setup snmp connection
 func SetupSnmpConnection(config *config.Config) (*gosnmp.GoSNMP, error) {
 	target := &gosnmp.GoSNMP{
 		Target:    config.SnmpCfg.Ip,
-		Port:      161,
+		Port:      config.SnmpCfg.Port,
 		Community: config.SnmpCfg.Community,
 		Version:   gosnmp.Version2c,
 		Timeout:   time.Duration(300) * time.Second,
-		Retries:   3,
+		//Logger:    gosnmp.NewLogger(log.New(os.Stdout, "", 0)),
+		AppOpts: make(map[string]interface{}),
 	}
 
 	err := target.Connect()
 	if err != nil {
 		return nil, fmt.Errorf("gagal terhubung: %w", err)
+	}
+
+	// Function handles for collecting metrics on query latencies.
+	var sent time.Time
+	target.OnSent = func(x *gosnmp.GoSNMP) {
+		sent = time.Now()
+	}
+	target.OnRecv = func(x *gosnmp.GoSNMP) {
+		log.Println("Query latency in seconds:", time.Since(sent).Seconds())
+	}
+	target.OnFinish = func(x *gosnmp.GoSNMP) {
+		log.Println("Total query latency in seconds:", time.Since(sent).Seconds())
 	}
 
 	return target, nil
