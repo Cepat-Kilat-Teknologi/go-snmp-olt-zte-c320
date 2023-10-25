73394abf70021cfc44dbca89004bddad966c6e81
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-25 11:17:24 +0700

    add get getnext bulkwalk bulkwalkall

--- internal/repository/snmp.go
+++ internal/repository/snmp.go
@@ -5,10 +5,12 @@ import (
 )
 
 type SnmpInterface interface {
-	GetSnmp() *gosnmp.GoSNMP
-	Connect() error
-	GetIpOlt() (string, error)
 	Walk(oid string, walkFunc func(pdu gosnmp.SnmpPDU) error) error
+	WalkAll(oid string) ([]gosnmp.SnmpPDU, error)
+	BulkWalk(oid string, walkFunc func(pdu gosnmp.SnmpPDU) error) error
+	BulkWalkAll(oid string) ([]gosnmp.SnmpPDU, error)
+	Get(oids []string) (result *gosnmp.SnmpPacket, err error)
+	GetNext(oids []string) (result *gosnmp.SnmpPacket, err error)
 }
 
 type snmpRepository struct {
@@ -21,18 +23,26 @@ func NewPonRepository(snmp *gosnmp.GoSNMP) SnmpInterface {
 	}
 }
 
-func (r *snmpRepository) GetSnmp() *gosnmp.GoSNMP {
-	return r.snmp
-}
-
-func (r *snmpRepository) Connect() error {
-	return r.snmp.Connect()
-}
-
-func (r *snmpRepository) GetIpOlt() (string, error) {
-	return r.snmp.Target, nil
-}
-
 func (r *snmpRepository) Walk(oid string, walkFunc func(pdu gosnmp.SnmpPDU) error) error {
 	return r.snmp.Walk(oid, walkFunc)
+}
+
+func (r *snmpRepository) WalkAll(oid string) ([]gosnmp.SnmpPDU, error) {
+	return r.snmp.WalkAll(oid)
+}
+
+func (r *snmpRepository) BulkWalk(oid string, walkFunc func(pdu gosnmp.SnmpPDU) error) error {
+	return r.snmp.BulkWalk(oid, walkFunc)
+}
+
+func (r *snmpRepository) BulkWalkAll(oid string) ([]gosnmp.SnmpPDU, error) {
+	return r.snmp.BulkWalkAll(oid)
+}
+
+func (r *snmpRepository) Get(oids []string) (result *gosnmp.SnmpPacket, err error) {
+	return r.snmp.Get(oids)
+}
+
+func (r *snmpRepository) GetNext(oids []string) (result *gosnmp.SnmpPacket, err error) {
+	return r.snmp.GetNext(oids)
 }
