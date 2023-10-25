1d9adf9002b0f2f67d6ba3baa515df4d4675c636
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-25 12:47:30 +0700

    delete AppOpts

--- pkg/snmp/snmp.go
+++ pkg/snmp/snmp.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/gosnmp/gosnmp"
 	"github.com/megadata-dev/go-snmp-olt-zte-c320/config"
-	"log"
 	"time"
 )
 
@@ -17,24 +16,11 @@ func SetupSnmpConnection(config *config.Config) (*gosnmp.GoSNMP, error) {
 		Version:   gosnmp.Version2c,
 		Timeout:   time.Duration(300) * time.Second,
 		//Logger:    gosnmp.NewLogger(log.New(os.Stdout, "", 0)),
-		AppOpts: make(map[string]interface{}),
 	}
 
 	err := target.Connect()
 	if err != nil {
 		return nil, fmt.Errorf("gagal terhubung: %w", err)
-	}
-
-	// Function handles for collecting metrics on query latencies.
-	var sent time.Time
-	target.OnSent = func(x *gosnmp.GoSNMP) {
-		sent = time.Now()
-	}
-	target.OnRecv = func(x *gosnmp.GoSNMP) {
-		log.Println("Query latency in seconds:", time.Since(sent).Seconds())
-	}
-	target.OnFinish = func(x *gosnmp.GoSNMP) {
-		log.Println("Total query latency in seconds:", time.Since(sent).Seconds())
 	}
 
 	return target, nil
