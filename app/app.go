53bd5aac0ee8eb21806574c21f0ac4974b818189
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-29 18:58:37 +0700

    add context snmpConn

--- app/app.go
+++ app/app.go
@@ -25,10 +25,11 @@ func New() *App {
 }
 
 func (a *App) Start(ctx context.Context) error {
-	// Get config path
+
+	// Get config path from APP_ENV environment variable
 	configPath := utils.GetConfigPath(os.Getenv("APP_ENV"))
 
-	// Load config
+	// Load configuration file from config path
 	cfg, err := config.LoadConfig(configPath)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to load config")
@@ -54,20 +55,29 @@ func (a *App) Start(ctx context.Context) error {
 	}(redisClient)
 
 	// Initialize SNMP connection
-	snmpConn, err := snmp.SetupSnmpConnection(cfg)
+	snmpConn, err := snmp.SetupSnmpConnection(ctx, cfg)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to setup SNMP connection")
 	}
 
 	// Check SNMP connection
-	err = snmpConn.Connect()
-	if err != nil {
+	/*
+		if SNMP Connection with wrong credentials in SNMP v3, return error is nil
+		if SNMP Connection with wrong Port in SNMP v2 v2c, return error is nil
+		if SNMP Connection with wrong community v2 v2c, return error is nil
+
+		Connect creates and opens a socket. Because UDP is a connectionless protocol,
+		you won't know if the remote host is responding until you send packets.
+		Neither will you know if the host is regularly disappearing and reappearing.
+	*/
+
+	if snmpConn.Connect() != nil {
 		log.Error().Err(err).Msg("Failed to connect to SNMP server")
 	} else {
 		log.Info().Msg("SNMP server successfully connected")
 	}
 
-	// Close SNMP connection
+	// Close SNMP connection after application shutdown
 	defer func() {
 		if err := snmpConn.Conn.Close(); err != nil {
 			log.Error().Err(err).Msg("Failed to close SNMP connection")
@@ -95,7 +105,7 @@ func (a *App) Start(ctx context.Context) error {
 	}
 
 	// Start server at given address
-	log.Info().Msgf("Server is successfully running")
+	log.Info().Msgf("Application started at %s", addr)
 
 	// Graceful shutdown
 	return graceful.Shutdown(ctx, server)
