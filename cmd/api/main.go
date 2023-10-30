9adfde99b82e382feaf6dd7885aa1731941819d6
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-30 20:46:23 +0700

    fix ci lint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,28 +7,26 @@ import (
 )
 
 func main() {
-
 	// Initialize application
 	server := app.New()                                     // Create a new instance of application
 	ctx, cancel := context.WithCancel(context.Background()) // Create a new context with cancel function
-	defer cancel()                                          // Cancel context when main function is finished
+	defer cancel()                                          // Cancel context when the main function is finished
 
 	// Start application server in a goroutine
 	go func() {
-		err := server.Start(ctx) // Start application server
+		err := server.Start(ctx) // Start the application server
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to start server") // Log error message
-			cancel()                                           // Cancel context if error occurred
+			cancel()                                           // Cancel context if an error occurred
 		}
 	}()
 
-	// Handle OS signals or other triggers for graceful shutdown if needed.
-	// For example, you can use a signal package like os/signal to capture
-	// and handle interrupt signals (e.g., Ctrl+C).
+	// Create a channel to wait for a signal to stop the application
+	stopSignal := make(chan struct{})
 
-	// Wait for a signal to gracefully stop the application.
-	select {
-	case <-ctx.Done(): // Application was gracefully stopped or an error occurred
-		log.Error().Err(ctx.Err()).Msg("Application was gracefully stopped or an error occurred")
-	}
+	// You can replace the select statement with a simple channel receive
+	<-stopSignal
+
+	// Log that the application is stopping
+	log.Info().Msg("Application is stopping")
 }
