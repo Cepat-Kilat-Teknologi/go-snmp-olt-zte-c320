a1e641d8030bc9af75e78492b0026a8a1adca68b
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-12-07 18:27:44 +0700

    add logSnmp with env

--- pkg/snmp/snmp.go
+++ pkg/snmp/snmp.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gosnmp/gosnmp"
 	"github.com/megadata-dev/go-snmp-olt-zte-c320/config"
 	"github.com/megadata-dev/go-snmp-olt-zte-c320/internal/utils"
+	"log"
 	"os"
 	"time"
 )
@@ -14,6 +15,7 @@ var (
 	snmpHost      string
 	snmpPort      uint16
 	snmpCommunity string
+	logSnmp       gosnmp.Logger
 )
 
 // SetupSnmpConnection is a function to set up snmp connection
@@ -23,20 +25,22 @@ func SetupSnmpConnection(ctx context.Context, config *config.Config) (*gosnmp.Go
 		snmpHost = os.Getenv("SNMP_HOST")
 		snmpPort = utils.ConvertStringToUint16(os.Getenv("SNMP_PORT"))
 		snmpCommunity = os.Getenv("SNMP_COMMUNITY")
+		logSnmp = gosnmp.Logger{}
 	} else {
 		snmpHost = config.SnmpCfg.Ip
 		snmpPort = config.SnmpCfg.Port
 		snmpCommunity = config.SnmpCfg.Community
+		logSnmp = gosnmp.NewLogger(log.New(os.Stdout, "", 0))
 	}
 
 	target := &gosnmp.GoSNMP{
 		Target:    snmpHost,
 		Port:      snmpPort,
 		Community: snmpCommunity,
-		Context:   ctx,
 		Version:   gosnmp.Version2c,
 		Timeout:   time.Duration(300) * time.Second,
-		//Logger:    gosnmp.NewLogger(log.New(os.Stdout, "", 0)),
+		Retries:   3,
+		Logger:    logSnmp,
 	}
 
 	err := target.Connect()
